2828bf2b4beb6c4267ffc4a6959d91e1722c0e18
light-shadow-cyber <rokitink23@gmail.com>
2025-04-12 20:18:14 +0900

    Add files via upload

---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,19 @@
+package kadai_018;
+
+public abstract class Kato_Chapter18 {
+	public String familyName = "加藤";
+	public String givenName;
+	public String address;
+	
+	public void commonIntroduce() {
+		System.out.println("住所は東京都中野区〇×です");
+	}
+	public abstract void eachIntroduce();
+	public void execIntroduce() {
+		System.out.println("名前は" + this.familyName + this.givenName + "です");
+		commonIntroduce();
+		eachIntroduce();
+		System.out.println("");
+	}
+	public abstract void setGivenName();
+}
